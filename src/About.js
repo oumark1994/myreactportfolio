375ef11ce988c01204a49d98948869ac207f72fa
oumark1994 <64393469+oumark1994@users.noreply.github.com>
2022-01-27 00:40:58 -0800

    Change the link

--- src/About.js
+++ src/About.js
@@ -35,7 +35,7 @@ const About = () => {
             </div>
             </div>  
             <div className='up__to__top__btn'>
-            <a href="#" className="bottom__to__top">
+            <a href="#Home" className="bottom__to__top">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
